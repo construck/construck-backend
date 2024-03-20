b18acdbb85e39014841a2f01e110984b69fba698
Gilles Kagarama <gilleskagarama@Gilless-MacBook-Pro.local>
2024-03-20 11:10:34 +0200

    Fix daily work revenue for non tipper trucks, should not prorate and duration should not be used

--- routes/workData.js
+++ routes/workData.js
@@ -1825,7 +1825,7 @@ router.get("/detailed/:canViewRevenues", async (req, res) => {
               expenditure: d.totalExpenditure,
               status: d.status,
               rate: d.rate,
-              comment: d.comment
+              comment: d.comment,
             };
           });
 
@@ -1857,7 +1857,6 @@ router.get("/detailed/:canViewRevenues", async (req, res) => {
         );
 
         datesPosted.map((dP) => {
-          console.log('@@@dPP1', dP)
           if (
             moment(Date.parse(dP.date)).isSameOrAfter(moment(startDate)) &&
             moment(Date.parse(dP.date)).isSameOrBefore(
