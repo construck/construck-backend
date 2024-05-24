283cfb59c12983e989b917aa499c0582fb6b21f7
Gilles Kagarama <gilleskagarama@KG---MacPro-14---2023.local>
2024-05-24 02:05:37 +0200

    improve authentication and register user

--- models/drivers.js
+++ models/drivers.js
@@ -1,18 +1,27 @@
 const mongoose = require("mongoose");
+const moment = require("moment");
+
 const DriverSchema = new mongoose.Schema(
   {
     title: {
       type: String,
     },
-    // user: {
-    //   type: mongoose.SchemaTypes.ObjectId,
-    //   transform: (v) => (v === "" ? null : v),
-    //   ref: "users",
-    // },
+    user: {
+      type: mongoose.SchemaTypes.ObjectId,
+      transform: (v) => (v === "" ? null : v),
+      ref: "users",
+    },
     employment: {
       type: String,
     },
-    
+    createdAt: {
+      type: Date,
+      default: moment(),
+    },
+    updatedAt: {
+      type: Date,
+      default: moment(),
+    },
   },
   { timestamp: true }
 );
