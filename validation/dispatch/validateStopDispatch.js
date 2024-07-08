7a02879750e3e784480afeacad9531f204ba8080
Gilles Kagarama <kagaramag@gmail.com>
2024-07-08 15:29:32 +0200

    validate inputs when stopping jobs, and block posting works from old version of the app

---
+++ validation/dispatch/validateStopDispatch.js
@@ -0,0 +1,22 @@
+const Joi = require("joi");
+
+const stopDispatchSchema = Joi.object({
+  duration: Joi.string().required(),
+  tripsDone: Joi.string(),
+  comment: Joi.string().allow(null, ""),
+  stoppedBy: Joi.string().required(),
+  postingDate: Joi.string().required(),
+  fuel: Joi.string().required(),
+  startIndex: Joi.string().required(),
+  endIndex: Joi.string().required(),
+});
+
+const validateStopDispatch = (body) => {
+  const validationResult = stopDispatchSchema.validate(body);
+  if (validationResult.error) {
+    return validationResult.error.details[0].message;
+  }
+  return null;
+};
+
+module.exports = validateStopDispatch;
